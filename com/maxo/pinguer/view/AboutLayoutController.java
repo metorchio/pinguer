da48050b08be9bd129db058ac9c1db1901a91616
mx.torchio@gmail.com <mx.torchio@gmail.com@e8ebddae-d0a2-a73e-86ad-7758e04b1b93>
2015-02-13 19:51:29 +0000

    cambio BackgroundAbout para que funcione con .jar

--- com/maxo/pinguer/view/AboutLayoutController.java
+++ com/maxo/pinguer/view/AboutLayoutController.java
@@ -1,14 +1,20 @@
 package com.maxo.pinguer.view;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class AboutLayoutController 
+public class AboutLayoutController
 {
 	private RootLayoutController mainWindow;
 	
 	private Stage aboutLayoutStage;
+	
+	@FXML
+	private ImageView background;
 	
 	@FXML
 	private Button exit;
@@ -27,5 +33,12 @@ public class AboutLayoutController
     public void setMainWindow(RootLayoutController mainWindow)
     {
         this.mainWindow = mainWindow;
+    }
+    
+    public void setBackground()
+    {
+		Image backgroundImage = new Image( getClass().getResourceAsStream("Duende_fumon.png") );
+		//System.out.println( getClass().getResourceAsStream("Duende_fumon.png") );
+    	background.setImage( backgroundImage ); 	
     }
 }
